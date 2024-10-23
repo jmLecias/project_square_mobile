eaddba0c80cb518bef54839fa00dc27fdb8788d2
jmLecias <johnmarklecias.lsd@gmail.com>
2024-10-23 10:03:47 +0800

    discover cameras second attempt

--- src/components/DiscoverCameras.js
+++ src/components/DiscoverCameras.js
@@ -1,61 +1,86 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text, Button } from 'react-native';
 import dgram from 'react-native-udp';
-
-
+import Zeroconf from 'react-native-zeroconf';
 
 const DiscoverCameras = () => {
-
     const [devices, setDevices] = useState([]);
 
     useEffect(() => {
-        const socket = dgram.createSocket('udp4');
+        // UDP (SSDP) Camera Discovery
+        // const socket = dgram.createSocket('udp4');
 
-        socket.bind(1901, (error) => {
-            if (error) {
-                console.error('Socket binding error:', error);
-                return;
-            }
-            console.log('Socket bound to port 1901');
+        // socket.bind(1901, (error) => {
+        //     if (error) {
+        //         console.error('Socket binding error:', error);
+        //         return;
+        //     }
+        //     console.log('Socket bound to port 1901');
 
-            const message = `
-                M-SEARCH * HTTP/1.1\r\n
-                HOST: 239.255.255.250:1900\r\n
-                MAN: "ssdp:discover"\r\n
-                MX: 1\r\n
-                ST: urn:schemas-upnp-org:device:DigitalSecurityCamera:1\r\n
-            `;
+        //     const message = `
+        //         M-SEARCH * HTTP/1.1\r\n
+        //         HOST: 239.255.255.250:1900\r\n
+        //         MAN: "ssdp:discover"\r\n
+        //         MX: 1\r\n
+        //         ST: urn:schemas-upnp-org:device:DigitalSecurityCamera:1\r\n
+        //     `;
 
-            socket.send(
-                message,
-                0,
-                message.length,
-                1900,
-                '239.255.255.250',
-                (err) => {
-                    if (err) console.error('Error sending SSDP message:', err);
-                    else console.log('SSDP search message sent');
-                }
-            );
-        });
+        //     socket.send(
+        //         message,
+        //         0,
+        //         message.length,
+        //         1900,
+        //         '239.255.255.250',
+        //         (err) => {
+        //             if (err) console.error('Error sending SSDP message:', err);
+        //             else console.log('SSDP search message sent');
+        //         }
+        //     );
+        // });
 
-        socket.on('message', (msg, rinfo) => {
-            console.log('Received message from:', rinfo.address);
-            console.log('Message:', msg);
+        // socket.on('message', (msg, rinfo) => {
+        //     console.log('Received message from:', rinfo.address);
+        //     console.log('Message:', msg);
 
-            // Extract relevant information from the response
-            const deviceInfo = parseSSDPResponse(msg.toString());
+        //     const deviceInfo = parseSSDPResponse(msg.toString());
+        //     setDevices((prevDevices) => [...prevDevices, deviceInfo]);
+        // });
+
+        const zeroconf = new Zeroconf();
+
+        const handleStart = () => console.log('Zeroconf: Started scanning');
+        const handleFound = (name) => console.log('Zeroconf: Found device:', name);
+        const handleResolved = (service) => {
+            console.log('Zeroconf: Resolved service:', service);
+            const deviceInfo = {
+                serviceType: service.type,
+                location: service.addresses[0] + ':' + service.port,
+            };
             setDevices((prevDevices) => [...prevDevices, deviceInfo]);
-        });
+        };
+        const handleError = (err) => console.error('Zeroconf error:', err);
 
+        // Register event listeners
+        zeroconf.on('start', handleStart);
+        zeroconf.on('found', handleFound);
+        zeroconf.on('resolved', handleResolved);
+        zeroconf.on('error', handleError);
+
+        // Start Zeroconf scanning
+        zeroconf.scan(type = 'http', protocol = 'tcp', domain = 'local.');
+
+        // Cleanup listeners and stop scanning on unmount
         return () => {
-            socket.close();
+            // socket.stop();
+            zeroconf.off('start', handleStart);
+            zeroconf.off('found', handleFound);
+            zeroconf.off('resolved', handleResolved);
+            zeroconf.off('error', handleError);
+            zeroconf.stop();
         };
     }, []);
 
     const parseSSDPResponse = (message) => {
-        // Parse the SSDP response to extract information like location, device type, etc.
-        // This function will vary depending on the format of the response you expect
         const lines = message.split('\r\n');
         const device = {};
         lines.forEach(line => {
@@ -71,7 +96,7 @@ const DiscoverCameras = () => {
     return (
         <View>
             <Text>IP Camera Discovery</Text>
-            <Button title="Search for Devices" onPress={() => { }} />
+            {/* <Button title="Search for Devices" onPress={() => { }} /> */}
             {devices.map((device, index) => (
                 <View key={index}>
                     <Text>Device: {device.serviceType}</Text>
